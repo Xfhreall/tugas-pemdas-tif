6198bd2966dad0b1b21b16a828635d7451ad849e
Risqi Fahreal <farelrizqi377@gmail.com>
2023-10-13 08:36:26 +0700

    add folder latihan

---
+++ source code/latihan/jastip/src/App.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        DecimalFormatSymbols symbols = new DecimalFormatSymbols();
+        symbols.setGroupingSeparator('.');
+        symbols.setDecimalSeparator(',');
+        DecimalFormat df = new DecimalFormat("Rp#,###.00", symbols);
+        Scanner input = new Scanner(System.in);
+
+        int harga = input.nextInt();
+        String jasa = input.next();
+
+        if (jasa.equalsIgnoreCase("Kirim")) {
+            double diskon = harga + ((double) harga * 0.1);
+
+            System.out.printf("\nTotal harga belanja : %s\n", df.format(harga));
+            System.out.printf("Status delivery : %s\n", "Diantarkan");
+            System.out.printf("Total harga yang perlu dibayar : %s", df.format(diskon));
+        } else if (jasa.equalsIgnoreCase("Ambil")) {
+            System.out.printf("\nTotal harga belanja : %s\n", df.format(harga));
+            System.out.printf("Status delivery : %s", "Diambil");
+        } else {
+            System.out.println("Yang bener ae lu");
+        }
+
+        input.close();
+    }
+}
--- source code/praktikum pemdas/praktikum3/src/App.java
+++ source code/praktikum pemdas/praktikum3/src/App.java
@@ -1,24 +1,22 @@
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
-import java.util.*;
+import java.util.*; 
+
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
         // soal no 1
-        // MENGHITUNG BMI
-
+        
         double bmi;
         System.out.println("BMI Calculator");
         System.out.print("Input your weight (kg) : ");
         double berat = input.nextDouble();
-
-        System.out.print("Input your height (m) : ");
+        System.out.print("input your height (m) : ");
         double tinggi = input.nextDouble();
 
         // operator
-
         bmi = berat / ((double) tinggi * (double) tinggi);
 
         if (bmi > 30) {
@@ -44,7 +42,6 @@ public class App {
         System.out.println("4. Upah lembur berjumlah Rp60.000,-/jam.");
         System.out.println("5. Denda kerja berjumlah Rp10.000,-/jam.");
         System.out.println("=".repeat(47));
-
         System.out.printf("Masukkan jumlah jam kerja\t: ");
         int jamKerja = input.nextInt();
         System.out.println("=".repeat(47));
